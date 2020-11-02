cd76cada2329fabee85849d964c33918438a3b04
Matthew Charlston <matthew@mcharlsto.uk>
2020-11-02 01:56:45 +0000

    Add functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, QtCore, uic
 import sys
+import random
+import time
+import RPi.GPIO as GPIO
+import hcsr04sensor
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -8,6 +12,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         uic.loadUi('mainwindow.ui', self)
 
+        self.ultrasonicTimer = QtCore.QTimer()
+        self.ultrasonicTimer.timeout.connect(lambda: ultrasonicPoll(self, 22, 12, 23, 1))
+
+        self.enableUltrasonicPoll.clicked.connect(self.toggleUltrasonicTimer)
         self.doAThing.clicked.connect(self.buttonFunction)
         self.actionQuit.triggered.connect(self.closeApp)
 
@@ -24,11 +32,38 @@ class MainWindow(QtWidgets.QMainWindow):
         elif textState == "Changed":
             self.mwLabel.setText("Hello World!")
             textState = "Default"
+    
+
+
+    def toggleUltrasonicTimer(self):
+        if self.ultrasonicTimer.isActive() == False:
+            self.ultrasonicTimer.start(500)
+        else:
+            self.ultrasonicTimer.stop()
+
 
     def closeApp(self):
         sys.exit()
 
+def gpioInit():
+    GPIO.setmode(GPIO.BCM) # Set mode to BCM numbering
+    
+def ultrasonicPoll(self, trig, echo, trig2, echo2):
+    '''
+    sensor1 = hcsr04sensor.sensor.Measurement(trig, echo) # Init both sensors
+    sensor2 = hcsr04sensor.sensor.Measurement(trig2, echo2)
+    distance1 = sensor1.raw_distance() # Get raw distance readings
+    distance2 = sensor2.raw_distance()
+    '''
+
+    distance1 = random.randint(0, 999) # Fake values while we're not on actual HW
+    distance2 = random.randint(0, 999)
+
+    self.distanceValue1.setText(str(round(distance1)) + " cm") # Set labels back in Qt GUI
+    self.distanceValue2.setText(str(round(distance2)) + " cm")
+
 def main():
+    gpioInit()
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
