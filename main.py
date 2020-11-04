fdd30ecdd9e3f48508d7886b64b32ed29c94d58a
Matthew Charlston <matthew@mcharlsto.uk>
2020-11-04 19:16:41 +0000

    Some stuff

--- main.py
+++ main.py
@@ -26,6 +26,49 @@ if os.uname()[1] == 'cryptoid':
     import hcsr04sensor
     import serial
 
+def ultrasonicPoll(self, trig, echo, trig2, echo2):
+    '''
+    sensor1 = hcsr04sensor.sensor.Measurement(trig, echo) # Init both sensors
+    sensor2 = hcsr04sensor.sensor.Measurement(trig2, echo2)
+    distance1 = sensor1.raw_distance() # Get raw distance readings
+    distance2 = sensor2.raw_distance()
+    '''
+
+    distance1 = random.randint(0, 999) # Fake values while we're not on actual HW
+    distance2 = random.randint(0, 999)
+
+    self.distanceValue1.setText(str(round(distance1)) + " cm") # Set labels back in Qt GUI
+    self.distanceValue2.setText(str(round(distance2)) + " cm")
+
+def setSTM32Text(self, state):
+    if state == True:
+        self.stm32Connected.setText("STM32 Connected") # Set text + colour
+        self.stm32Connected.setStyleSheet("color:#33cc33")
+    elif state == False:
+        self.stm32Connected.setText("STM32 Disconnected")
+        self.stm32Connected.setStyleSheet("color:#ff0000")
+
+def beepBuzzer(self, noteList):
+    notes = {
+        "A": 440,
+        "B": 494,
+        "C": 523,
+        "D": 587,
+        "E": 659,
+        "F": 698,
+        "G": 784
+    }
+    for x in noteList:
+        freq = notes[x]
+        print(freq)
+        buzzerPWM.ChangeFrequency(freq)
+
+def gpioInit(self):
+    GPIO.setmode(GPIO.BCM) # Set mode to BCM numbering
+    GPIO.setup(18, GPIO.OUT)
+    buzzerPWM = GPIO.PWM(18, 0)
+    buzzerPWM.ChangeDutyCycle(20)
+
 class MainWindow(QtWidgets.QMainWindow):
 
     def buttonFunction(self):
@@ -56,8 +99,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         uic.loadUi('mainwindow.ui', self)
 
-        '''GPIO.setmode(GPIO.BCM) # Set mode to BCM numbering
-        stm32 = serial.Serial('/dev/ttyS0', 115200, parity=serial.PARITY_EVEN, timeout=1) # Open serial comms with the STM32
+        '''stm32 = serial.Serial('/dev/ttyS0', 115200, parity=serial.PARITY_EVEN, timeout=1) # Open serial comms with the STM32
         stm32.write("INIT\n") # Init the STM32
         while True: # Check for response
             response = stm32.readline()
@@ -65,35 +107,16 @@ class MainWindow(QtWidgets.QMainWindow):
                 print("STM32 is working")
                 setSTM32Text(self, True)
                 break'''
-
+        
+        gpioInit(self)
+        beepBuzzer(self, ["A", "D", "E"])
+        
         self.ultrasonicTimer = QtCore.QTimer()
         self.ultrasonicTimer.timeout.connect(lambda: ultrasonicPoll(self, 22, 12, 23, 1))
 
         self.enableUltrasonicPoll.clicked.connect(self.toggleUltrasonicTimer)
         self.doAThing.clicked.connect(self.buttonFunction)
         self.actionQuit.triggered.connect(self.closeApp)
-
-def ultrasonicPoll(self, trig, echo, trig2, echo2):
-    '''
-    sensor1 = hcsr04sensor.sensor.Measurement(trig, echo) # Init both sensors
-    sensor2 = hcsr04sensor.sensor.Measurement(trig2, echo2)
-    distance1 = sensor1.raw_distance() # Get raw distance readings
-    distance2 = sensor2.raw_distance()
-    '''
-
-    distance1 = random.randint(0, 999) # Fake values while we're not on actual HW
-    distance2 = random.randint(0, 999)
-
-    self.distanceValue1.setText(str(round(distance1)) + " cm") # Set labels back in Qt GUI
-    self.distanceValue2.setText(str(round(distance2)) + " cm")
-
-def setSTM32Text(self, state):
-    if state == True:
-        self.stm32Connected.setText("STM32 Connected") # Set text + colour
-        self.stm32Connected.setStyleSheet("color:#33cc33")
-    elif state == False:
-        self.stm32Connected.setText("STM32 Disconnected")
-        self.stm32Connected.setStyleSheet("color:#ff0000")
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
