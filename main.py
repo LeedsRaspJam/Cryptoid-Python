3531767572bcd39d5fb54a2129119f62a114329e
Matthew Charlston <matthew@mcharlsto.uk>
2021-01-11 21:37:04 +0000

    Move back into one file

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ import sys
 import random
 import os
 import time
-from cryptoidHAL import *
 
 if os.uname()[1] == 'cryptoid':
     import RPi.GPIO as GPIO
     from hcsr04sensor import sensor
+    import serial
 
 def ultrasonicPoll(self, trig, echo, trig2, echo2):
     sensor1 = sensor.Measurement(trig, echo) # Init both sensors
@@ -35,6 +35,79 @@ def ultrasonicPoll(self, trig, echo, trig2, echo2):
 
     self.distanceValue1.setText(str(round(distance1)) + " cm") # Set labels back in Qt GUI
     self.distanceValue2.setText(str(round(distance2)) + " cm")
+
+def beepSPKR(self, freq, duration):
+    while True:
+        self.logTb.append("SPKR")
+        stm32.write("SPKR\r\n".encode())
+        response = stm32.readline()
+        self.logTb.append(str(response))
+        if response.decode() == "OK\r\n":
+            stm32.write(str(freq).encode())
+            stm32.write("\r\n".encode())
+            response = stm32.readline()
+            self.logTb.append(str(response))
+            if response.decode() == "OK\r\n":
+                stm32.write(str(duration).encode())
+                stm32.write("\r\n".encode())
+                response = stm32.readline()
+                self.logTb.append(str(response))
+                if response.decode() == "OK\r\n":
+                    break
+
+def setLED(self, ledID, rValue, gValue, bValue):
+    if ledID == "all": # If setting all LEDs
+        while True: # Loop until all data sent
+            self.logTb.append("LEDA")
+            stm32.write("LEDA\r\n".encode()) # Send command
+            response = stm32.readline() # Read response
+            self.logTb.append(str(response)) # Print response
+            if response.decode() == "OK\r\n": # If response recieved
+                stm32.write(str(rValue).encode()) # Send first set of data
+                stm32.write("\r\n".encode()) # Send newline
+                response = stm32.readline() # Get second response
+                self.logTb.append(str(response)) # Print second response
+                if response.decode() == "OK\r\n": # If response recieved
+                    stm32.write(str(gValue).encode()) # Send second set of data
+                    stm32.write("\r\n".encode()) # Send newline
+                    response = stm32.readline() # Get third response
+                    self.logTb.append(str(response)) # Print third response
+                    if response.decode() == "OK\r\n": # If response recieved
+                        stm32.write(str(bValue).encode()) # Send third set of data
+                        stm32.write("\r\n".encode()) # Send newline
+                        response = stm32.readline() # Get fourth response
+                        self.logTb.append(str(response)) # Print third response
+                        if response.decode() == "OK\r\n": # If response recieved
+                            break # Sent successfully, break from loop.
+
+    else: # If only setting one
+        while True:
+            self.logTb.append("LEDS")
+            stm32.write("LEDS\r\n".encode())
+            response = stm32.readline()
+            self.logTb.append(str(response))
+            if response.decode() == "OK\r\n":
+                stm32.write(ledID.encode())
+                stm32.write("\r\n".encode())
+                response = stm32.readline()
+                self.logTb.append(str(response))
+                if response.decode() == "OK\r\n":
+                    stm32.write(str(rValue).encode())
+                    stm32.write("\r\n".encode())
+                    response = stm32.readline()
+                    self.logTb.append(str(response))
+                    if response.decode() == "OK\r\n":
+                        stm32.write(str(gValue).encode())
+                        stm32.write("\r\n".encode())
+                        response = stm32.readline()
+                        self.logTb.append(str(response))
+                        if response.decode() == "OK\r\n":
+                            stm32.write(str(bValue).encode())
+                            stm32.write("\r\n".encode())
+                            response = stm32.readline()
+                            self.logTb.append(str(response))
+                            if response.decode() == "OK\r\n":
+                                break
 
 def setSTM32Text(self, state):
     if state == True:
@@ -76,8 +149,25 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.ultrasonicTimer.stop()
 
+    def resetSTM(self): # Send Software Reset instruction
+        stm32.write("RSTS\r\n".encode())
+        self.logTb.append("Resetting now...")
+        time.sleep(1.5)
+        stm32.reset_input_buffer()
+        self.logTb.append("Reset Complete")
+
     def clearLog(self): # Clear the log
         self.logTb.clear()
+    
+    def printVer(self): # Print STM version info
+        stm32.write("VERS\r\n".encode())
+        response = stm32.readline()
+        response2 = stm32.readline()
+        response3 = stm32.readline()
+
+        self.logTb.append(str(response))
+        self.logTb.append(str(response2))
+        self.logTb.append(str(response3))
 
     def closeApp(self):
         sys.exit()
@@ -86,18 +176,30 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         uic.loadUi('mainwindow.ui', self)
+
+        global stm32
+        stm32 = serial.Serial('/dev/ttyAMA0', 115200, parity=serial.PARITY_EVEN) # Open serial comms with the STM32
+        self.logTb.append("Trying to initialize the STM32")
+        while True: # Check for response
+            stm32.write("INIT\r\n".encode()) # Init the STM32
+            self.logTb.append("INIT")
+            response = stm32.readline()
+            self.logTb.append(str(response))
+            if response.decode() == "OK\r\n":
+                self.logTb.append("STM32 is working")
+                setSTM32Text(self, True)
+                break
         
         gpioInit(self)
-        halINIT(self)
-        
+
         self.ultrasonicTimer = QtCore.QTimer()
         self.ultrasonicTimer.timeout.connect(lambda: ultrasonicPoll(self, 22, 12, 23, 1))
 
         self.enableUltrasonicPoll.clicked.connect(self.toggleUltrasonicTimer)
         self.doAThing.clicked.connect(self.buttonFunction)
         self.clearBtn.clicked.connect(self.clearLog)
-        self.resetBtn.clicked.connect(resetSTM)
-        self.versBtn.clicked.connect(printVer)
+        self.resetBtn.clicked.connect(self.resetSTM)
+        self.versBtn.clicked.connect(self.printVer)
         self.actionQuit.triggered.connect(self.closeApp)
 
 def main():
