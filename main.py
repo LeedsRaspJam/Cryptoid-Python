596b2f7a614acc66c494d856e0879b9f189c0c1c
Matthew Charlston <matthew@mcharlsto.uk>
2021-01-12 00:50:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -204,6 +204,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.logTb.append("Resetting now...")
         time.sleep(1.5)
         stm32.reset_input_buffer()
+        setSTM32Text(False)
         self.logTb.append("Reset Complete")
 
     def clearLog(self): # Clear the log
