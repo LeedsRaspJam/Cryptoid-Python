075bedfd79d836d22f43c2979b99b94f88a606c5
Matthew Charlston <matthew@mcharlsto.uk>
2021-01-19 00:08:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ if os.uname()[1] == 'cryptoid':
 global motorBuffer, ledBuffer
 
 motorBuffer = {
-    "1"": [0 0]
-    "2"": [0 0]
-    "3"": [0 0]
-    "4"": [0 0]
+    "1": [0 0]
+    "2": [0 0]
+    "3": [0 0]
+    "4": [0 0]
 }
 
 ledBuffer = {
