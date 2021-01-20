964868a7467760fe8b5d9baeb63b849fb4e36038
Matthew Charlston <matthew@mcharlsto.uk>
2021-01-20 01:51:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -123,6 +123,10 @@ def controllerPoll(self):
     if isStopped == True:
         self.LBar.setValue(100)
         self.RBar.setValue(100)
+    elif l_value == 0:
+        self.LBar.setValue(100)
+    elif r_value = 0:
+        self.RBar.setValue(100)
     else:
         self.LBar.setValue(l_value)
         self.RBar.setValue(r_value)
