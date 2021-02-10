81dcf1dc033435a7599d5239f15cec3659027780
Matthew Charlston <matthew@mcharlsto.uk>
2021-02-10 12:26:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -461,7 +461,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def loadTask(self): # Load task into RAM
         currentTaskLocation = QtWidgets.QFileDialog.getOpenFileName(self, "Open Task", "tasks", "Cryptoid Task File (*.crtask)") # Get task location with file dialog
-
+        
         taskFile = open(currentTaskLocation, "r") # Open task file as object
         self.taskTextEdit.setPlainText(taskFile.read()) # Dump file to QTextEdit
         taskFile.close() # Close the file
@@ -476,10 +476,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
         taskFile = open(currentTaskLocation, "w") # Open task file as object
         taskFile.write("self.logTb.append(\"Example Text\"") # Write in example text
+        self.taskTextEdit.setPlainText(taskFile.read()) # Dump file to QTextEdit
         taskFile.close() # Close the file
 
     def saveTask(self): # Save task to SD
-        taskFile = open(currentTaskLocation, "w") # Open task file as object
+        taskFile = open(currentTaskLocation, "rw") # Open task file as object
         taskFile.write(self.taskTextEdit.toPlainText()) # Dump QTextEdit to file
         taskFile.close() # Close the file
 
