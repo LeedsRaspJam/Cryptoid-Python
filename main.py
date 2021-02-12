b7c5e008a44aa6b0f743595dc92e1e0cf1263b18
Matthew Charlston <matthew@mcharlsto.uk>
2021-02-12 06:46:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -504,7 +504,7 @@ class MainWindow(QtWidgets.QMainWindow):
             currentTaskLocation = "" # Clear currentTaskLocation
 
     def onTextUpdate(self): # Update highlighting + save file upon update
-        print(highlight(self.taskTextEdit.toPlainText(), PythonLexer(), RtfFormatter())) # Highlight text
+        self.taskTextEdit.setPlainText(highlight(self.taskTextEdit.toPlainText(), PythonLexer(), RtfFormatter())) # Highlight text
         taskFile = open(currentTaskLocation, "w") # Open task file as object
         taskFile.write(self.taskTextEdit.toPlainText()) # Dump QTextEdit to file
         taskFile.close() # Close the file
